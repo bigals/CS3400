8ae5c8c79de82bb47d94554f7aa3de494153b32e
bigals <bigalpromos@gmail.com>
2014-10-13 15:45:05 -0400

    pushing navbar chnages to index.html, still not completly done, needs more work.

---
+++ JsWebsite/Scripts/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    
+    $('#homeNav').hover(function() { //Hover In Event Handler
+    
+    }, function() { //Hover Out event Handler
+    
+    });
+    
+    $('#practNav').hover(function() { //Hover In Event Handler
+    
+    }, function() { //Hover Out event Handler
+    
+    });
+    
+    $('#testNav').hover(function() { //Hover In Event Handler
+    
+    }, function() { //Hover Out event Handler
+    
+    });
+    
+    $('#debugNav').hover(function() { //Hover In Event Handler
+    
+    }, function() { //Hover Out event Handler
+    
+    });
+});

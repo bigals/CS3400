f9d756ba257e235cd7d99ac5c2366d0473a6eb8c
bigals <bigalpromos@gmail.com>
2014-10-14 03:09:37 -0400

    Finsihed creation of the navBar to support the link directly to dropdown contentvia a query string parameter, and javascript/jquery magic, also added the remaining html files needed for wednesday.

--- JsWebsite/Scripts/scripts.js
+++ JsWebsite/Scripts/scripts.js
@@ -4,9 +4,30 @@ function openPane(btnSelector, panelSelector) {
     $(panelSelector).show();
 }
 
+function getQueryString() {
+    var currentURL = window.location.href;  //Get the URL of the current window location
+    var queryString = currentURL.split('?')[1];    //Split out the query string from the URL
+    var selectors = ['#cNav-', '#content-'];    //Screate array to hold values to be returned with their base values
+    var keyValPairs = queryString.split('=');   //Split the key and value pair
+    
+    if(keyValPairs[0] === 'tOpVal') {       //Ensure that the correct key has been picked up
+        selectors[0] += keyValPairs[1];     //Set the selectors with the value from the key picked up
+        selectors[1] += keyValPairs[1];
+    }
+    
+    //If value from query string was not picked up, then set the selectors to the default
+    if ((selectors[0] === '#cNav-') && (selectors[1] === '#content-')) {
+        selectors[0] += '1';
+        selectors[1] += '1';
+    }
+    
+    return selectors;
+}
+
 $(document).ready(function() {
     
-    openPane('#cNav-1', '#content-1');
+    var selectorArr = getQueryString();
+    openPane(selectorArr[0], selectorArr[1]);
     
     //Click event handler for the left bar of content nav buttons
     $("div.contentBtn").click(function() {

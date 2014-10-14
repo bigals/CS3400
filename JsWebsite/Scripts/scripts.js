62a4371bdeb8daf871933ea90594c484749421d8
bigals <bigalpromos@gmail.com>
2014-10-14 10:24:50 -0400

    added WebGL example to the examples.html page, and added storage example as well. Updated nav bar on all pages with new links to panels that have been created.

--- JsWebsite/Scripts/scripts.js
+++ JsWebsite/Scripts/scripts.js
@@ -109,12 +109,256 @@ function checkAndRenderCanvas() {
     }
 }
 
+function loadFileAsText()
+{
+	var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+	var fileReader = new FileReader();
+	fileReader.onload = function(fileLoadedEvent) 
+	{
+		var textFromFileLoaded = fileLoadedEvent.target.result;
+		
+        //Output to Screen		
+		document.getElementById("ldedFile").value = textFromFileLoaded;		
+		
+	};
+	fileReader.readAsText(fileToLoad, "UTF-8");
+}
+
+function init() {
+    var canvas = document.getElementById('drawSurface'); // A static variable, due to the fact that one or more local functions access it.
+    var context = canvas.getContext('2d'); // A static variable, due to the fact that one or more local functions access it.
+
+    context.fillStyle = "purple";
+
+    if (window.navigator.msPointerEnabled) {
+        canvas.addEventListener('MSPointerMove', paintCanvas, false);
+    }
+    else {
+        canvas.addEventListener('mousemove', paintCanvas, false);
+    }
+
+    document.getElementById('erase').addEventListener('click', eraseCanvas, false);
+    document.getElementById('save').addEventListener('click', saveCanvas, false);
+    document.getElementById('load').addEventListener('click', loadCanvas, false);
+
+    function paintCanvas(event) { // The "event" object contains the position of the pointer/mouse.
+        context.fillRect(event.offsetX, event.offsetY, 4, 4); // Draw a 4x4 rectangle at the given coordinates (relative to the canvas box). As of this writing, not all browsers support offsetX and offsetY.
+    } // paintCanvas
+
+    function saveCanvas() {
+        window.localStorage.canvasImage = canvas.toDataURL(); // Save the user's drawing to persistent local storage.
+    } // saveCanvas
+
+    function eraseCanvas() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    } // eraseCanvas
+
+    function loadCanvas() {
+        var img = new Image(); // The canvas drawImage() method expects an image object.
+
+        img.src = window.localStorage.canvasImage; // Retrieve the last saved artistic achievement from persistent local storage.
+        img.onload = function() { // Only render the saved drawing when the image object has fully loaded the drawing into memory.
+            context.drawImage(img, 0, 0); // Draw the image starting at canvas coordinate (0, 0) - the upper left-hand corner of the canvas.
+        } // onload
+    } // loadCanvas
+} // init
+
+function checkAndInitLStoreCanv() {
+    if($('#content-2').hasClass('htmlStorCanv')) {
+        window.addEventListener('load', init, false); // Safety first.        
+    }
+}
+
+function checkAndCreateGLCube() {
+    if($('#content-6').hasClass('webGLDemo'))
+    {
+        var CANVAS=document.getElementById("rotCube");
+        //CANVAS.width=window.innerWidth;
+        //CANVAS.height=window.innerHeight;
+
+        /*========================= GET WEBGL CONTEXT ========================= */
+        try {
+            var GL = CANVAS.getContext("experimental-webgl", {antialias: true});
+        } catch (e) {
+            alert("You are not webgl compatible :(") ;
+            return false;
+        } ;
+
+        /*========================= SHADERS ========================= */
+
+        var shader_vertex_source="\n\
+        attribute vec3 position;\n\
+        uniform mat4 Pmatrix;\n\
+        uniform mat4 Vmatrix;\n\
+        uniform mat4 Mmatrix;\n\
+        attribute vec3 color; //the color of the point\n\
+        varying vec3 vColor;\n\
+        void main(void) { //pre-built function\n\
+        gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);\n\
+        vColor=color;\n\
+        }";
+
+        var shader_fragment_source="\n\
+        precision mediump float;\n\
+        varying vec3 vColor;\n\
+        void main(void) {\n\
+        gl_FragColor = vec4(vColor, 1.);\n\
+        }";
+
+        var get_shader=function(source, type, typeString) {
+        var shader = GL.createShader(type);
+        GL.shaderSource(shader, source);
+        GL.compileShader(shader);
+        if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+          alert("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader));
+          return false;
+        }
+        return shader;
+        };
+
+        var shader_vertex=get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX");
+        var shader_fragment=get_shader(shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT");
+
+        var SHADER_PROGRAM=GL.createProgram();
+        GL.attachShader(SHADER_PROGRAM, shader_vertex);
+        GL.attachShader(SHADER_PROGRAM, shader_fragment);
+
+        GL.linkProgram(SHADER_PROGRAM);
+
+        var _Pmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Pmatrix");
+        var _Vmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Vmatrix");
+        var _Mmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Mmatrix");
+
+        var _color = GL.getAttribLocation(SHADER_PROGRAM, "color");
+        var _position = GL.getAttribLocation(SHADER_PROGRAM, "position");
+
+        GL.enableVertexAttribArray(_color);
+        GL.enableVertexAttribArray(_position);
+
+        GL.useProgram(SHADER_PROGRAM);
+
+        /*========================= THE CUBE ========================= */
+        //POINTS :
+        var cube_vertex=[
+        -1,-1,-1,
+
+        0,0,0,
+
+        1,-1,-1,
+
+        1,0,0,
+
+        1,1,-1,
+
+        1,1,0,
+
+        -1,1,-1,
+
+        0,1,0,
+
+        -1,-1,1,
+
+        0,0,1,
+
+        1,-1,1,
+
+        1,0,1,
+
+        1,1,1,
+
+        1,1,1,
+
+        -1,1,1,
+
+        0,1,1
+        ];
+
+        var CUBE_VERTEX= GL.createBuffer ();
+        GL.bindBuffer(GL.ARRAY_BUFFER, CUBE_VERTEX);
+        GL.bufferData(GL.ARRAY_BUFFER,
+                    new Float32Array(cube_vertex),
+        GL.STATIC_DRAW);
+
+        //FACES :
+        var cube_faces = [
+        0,1,2,
+        0,2,3,
+
+        4,5,6,
+        4,6,7,
+
+        0,3,7,
+        0,4,7,
+
+        1,2,6,
+        1,5,6,
+
+        2,3,6,
+        3,7,6,
+
+        0,1,5,
+        0,4,5
+
+        ];
+        var CUBE_FACES= GL.createBuffer ();
+        GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, CUBE_FACES);
+        GL.bufferData(GL.ELEMENT_ARRAY_BUFFER,
+                    new Uint16Array(cube_faces),
+        GL.STATIC_DRAW);
+
+        /*========================= MATRIX ========================= */
+
+        var PROJMATRIX=LIBS.get_projection(40, CANVAS.width/CANVAS.height, 1, 100);
+        var MOVEMATRIX=LIBS.get_I4();
+        var VIEWMATRIX=LIBS.get_I4();
+
+
+
+        LIBS.translateZ(VIEWMATRIX, -6);
+
+        /*========================= DRAWING ========================= */
+        GL.enable(GL.DEPTH_TEST);
+        GL.depthFunc(GL.LEQUAL);
+        GL.clearColor(0.0, 0.0, 0.0, 0.0);
+        GL.clearDepth(1.0);
+
+        var time_old=0;
+        var animate=function(time) {
+        var dt=time-time_old;
+        LIBS.rotateZ(MOVEMATRIX, dt*0.001);
+        LIBS.rotateY(MOVEMATRIX, dt*0.002);
+        LIBS.rotateX(MOVEMATRIX, dt*0.003);
+        time_old=time;
+
+        GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);
+        GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
+        GL.uniformMatrix4fv(_Pmatrix, false, PROJMATRIX);
+        GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
+        GL.uniformMatrix4fv(_Mmatrix, false, MOVEMATRIX);
+        GL.vertexAttribPointer(_position, 3, GL.FLOAT, false,4*(3+3),0) ;
+        GL.vertexAttribPointer(_color, 3, GL.FLOAT, false,4*(3+3),3*4) ;
+        GL.bindBuffer(GL.ARRAY_BUFFER, CUBE_VERTEX);
+        GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, CUBE_FACES);
+        GL.drawElements(GL.TRIANGLES, 6*2*3, GL.UNSIGNED_SHORT, 0);
+
+        GL.flush();
+
+        window.requestAnimationFrame(animate);
+        };
+        animate(0);
+    }
+}
+
 $(document).ready(function() {
     
     var selectorArr = getQueryString();
     openPane(selectorArr[0], selectorArr[1]);
     
+    //Run the Appropriate JavaScript for each of the Examples panels
+    checkAndInitLStoreCanv();
     checkAndRenderCanvas();
+    checkAndCreateGLCube();
     
     //Click event handler for the left bar of content nav buttons
     $("div.contentBtn").click(function() {
@@ -125,5 +369,9 @@ $(document).ready(function() {
         $('.contentBtn').removeClass('selected');
         
         openPane(btnSel, panelSel);
+    });
+    
+    $('#loadFileBtn').click(function() {
+        loadFileAsText();
     });
 });

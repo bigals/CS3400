618aa38ef83e68935c8ba0d5f8ff67fd48429516
mhb7766 <michael.w.hillenbrand@wmich.edu>
2014-10-15 00:05:29 -0400

    more examples added

--- JsWebsite/Scripts/scripts.js
+++ JsWebsite/Scripts/scripts.js
@@ -358,6 +358,13 @@ function mouseOut() {
     document.getElementById("demo").style.color = "black";
 }
 
+//highlight all text example
+function SelectAll(id)
+{
+    document.getElementById(id).focus();
+    document.getElementById(id).select();
+}
+
 //dropdown menu example
 var timeout = 500;
 var closetimer  = 0;
@@ -402,6 +409,42 @@ function mcancelclosetime()
 // close layer when click-out
 document.onclick = mclose;
 
+//add sub-options to a form example
+var otherStuff = {
+   "item 1" : [ "subitem 1.1", "subitem 1.2", "subitem 1.3", "subitem 1.4" ],
+   "item 2" : [ "subitem 2.1", "subitem 2.2" ],
+   "item 4" : [ "subitem 4" ],
+   "item 6" : [ "subitem 6.1", "subitem 6.2" ]
+};
+
+function selectAll(listName, selected) {
+  var listBox = document.getElementById(listName);
+  for(i=0; i<listBox.length; i++) {
+    listBox.options[i].selected=selected;
+  }
+  if( listBox.onchange ) {
+    listBox.onchange();
+  }
+}
+
+function lstStuff_OnChange() {
+  var listBox = document.getElementById("lstStuff");
+  var subListBox = document.getElementById("lstOtherStuff");
+  subListBox.options.length=0;
+  for(i=0; i<listBox.length; i++) {
+    if( listBox.options[i].selected ) {
+      var key = listBox.options[i].text;
+      if(otherStuff[key]) {
+        for(j=0; j<otherStuff[key].length; j++) {
+        subListBox.options.add(new Option(otherStuff[key][j],otherStuff[key][j]));
+        }
+      }
+    }
+  }
+}
+
+
+//JQuery
 $(document).ready(function() {
     
     var selectorArr = getQueryString();

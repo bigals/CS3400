da2ac748343339d7c25dcb9986c3dd0233046d63
Smitty10492 <John.matthew.smith2012@gmail.com>
2014-10-21 01:16:33 -0400

    Working Sliders, submit buttion needs work

---
+++ KineticSculptProject/createSlider.js
@@ -0,0 +1,41 @@
+var sliderCount = 0;
+var sliderValueArray = [];
+
+function grabSliderValues(){
+for( i=0; i<sliderCount; i++){
+var slideValue = $('#s'+i+'Slider').slider( "option", "value" );
+sliderValueArray[i] = slideValue;
+alert(sliderValueArray[i]);
+
+
+
+}
+sliderCount=0;
+};
+
+
+$(document).ready(function(){
+
+
+
+//Create sliders
+	$('.Slider').each(function(idx, elm) {
+    			var name = elm.id.replace('Slider', '');
+				sliderCount++;
+			$('#' + elm.id).slider({
+			        startValue: 0,
+		        	min: 0,
+		        	max: 179,
+		        	//step: .5,
+		        	slide: function(event, ui) {
+		        	    $('#' + name).val(ui.value);
+						
+						
+		        	}
+		    	});
+				
+		});
+		
+
+     });
+		

c1efd10e80a58ba34a7e10f5815f9405f876af61
mhb7766 <michael.w.hillenbrand@wmich.edu>
2014-10-22 14:39:14 -0400

    new edits, added canvas border

--- KineticSculptProject/scripts/mike_script.js
+++ KineticSculptProject/scripts/mike_script.js
@@ -1,35 +1,12 @@
-// do cool things with the context
-context.font = '40pt Calibri';
-context.fillStyle = 'blue';
-context.fillText('Hello World!', 150, 100);
-
-var line1 = {
-        name: "l1",
-        x0: x10,
-        y0: y10,
-        x: 0,
-        y: 0
-};
-
-var line2 = {
-	name: "l2",
-	x0: 10,
-	y0: 20,
-	x: 12,
-	y: 32
-};	
-
-function drawLine(myLine, context) {
-	context.beginPath();
-	context.moveTo(myLine.x0, myLine.y0);
-	context.lineTo(myLine.x, myLine.y);
-	context.strokeStyle = 'red';
-	context.stroke();
+function doThings(ctx) {
+	ctx.moveTo(0,0);
+	ctx.lineTo(200,100);
+	ctx.stroke();
 }
 
-$(document).ready(function (){
-    var canvs  = document.getElementById('myCanvas');
-    var context = canvs.getContext('2d');
 
-    drawLine(l2, context);
+$(document).ready(function() {
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	doThings(ctx);
 });

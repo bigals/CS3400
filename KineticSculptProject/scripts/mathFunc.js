779775a8cc9c54c899417cf21bf36b1bdd949338
bigals <bigalpromos@gmail.com>
2014-10-29 11:50:09 -0400

    Kinetic Sculpture Project complete

--- KineticSculptProject/scripts/mathFunc.js
+++ KineticSculptProject/scripts/mathFunc.js
@@ -1,23 +1,26 @@
 //Global Variables for physics characteristics
-var canvs
-var contxt;
-var time = 0.05;
-var g = 9.81;
-var length = 120;
-var mass = 10;
-var x10 = 345;
-var y10 = 180;
-var mpht = 6 / (mass * Math.pow(length, 2));
-var mpot = -0.5 * (mass * Math.pow(length, 2));
-var gOvL = g / length;
-var poten1 = 10;
-var poten2 = 30;
-var theta1 = 50 / 180 * Math.PI;
-var theta2 = 65 / 180 * Math.PI;
-var fOrdPoten1 = 0;
-var fOrdPoten2 = 0;
-var fOrdTheta1 = 0;
-var fOrdTheta2 = 0;
+var init = {};
+var time;
+var g;
+var length;
+var mass;
+var x10;
+var y10;
+var mpht;
+var mpot;
+var gOvL;
+var poten1;
+var poten2;
+var theta1;
+var theta2;
+var fOrdPoten1;
+var fOrdPoten2;
+var fOrdTheta1;
+var fOrdTheta2;
+var sOrdPoten1;
+var sOrdPoten2;
+var sOrdTheta1;
+var sOrdTheta2;
 
 //draws circle based on a context passed in, and our defined circle object thats passed in
 function drawCircle(myCircle, context) {
@@ -47,10 +50,10 @@ function drawLine(myLine, context) {
 function calcNxtVals() {
     fOrdTheta1 = mpht * (((2 * poten1) - (3 * poten2 * Math.cos(theta1 - theta2))) / (16 - (9 * Math.pow((Math.cos(theta1 - theta2)), 2))));
     fOrdTheta2 = mpht * (((8 * poten2) - (3 * poten1 * Math.cos(theta1 - theta2))) / (16 - (9 * Math.pow((Math.cos(theta1 - theta2)), 2))));
-    theta1 += (fOrdTheta1 * time);
-    theta2 += (fOrdTheta2 * time);
     fOrdPoten1 = mpot * ((fOrdTheta1 * fOrdTheta2 * Math.sin(theta1 - theta2)) + (3 * gOvL * Math.sin(theta1)));
     fOrdPoten2 = mpot * (-(fOrdTheta1 * fOrdTheta2 * Math.sin(theta1 - theta2)) + (gOvL * Math.sin(theta2)));
+    theta1 += (fOrdTheta1 * time);
+    theta2 += (fOrdTheta2 * time);
     poten1 += (fOrdPoten1 * time);
     poten2 += (fOrdPoten2 * time);
 }
@@ -78,7 +81,59 @@ function animatePendulum(circle1, circle2, line1, line2, canvas, canContext) {
     drawCircle(circle2, canContext);
 }
 
-function beginSim(canv, cxt, init) {
+function drawInitialBars(canv, cxt) {
+    var l = resizeCanvas(canv, cxt);
+    
+    clearInterval(init);
+    
+    getSliderVals(l);
+    
+    var line1 = {
+        name: "1",
+        x0: x10,
+        y0: y10,
+        x: 0,
+        y: 0
+    };
+    var line2 = {
+        name: "2",
+        x0: 0,
+        y0: 0,
+        x: 0,
+        y: 0
+    };
+    var circle1 = {
+        x: x10 + (length * Math.sin(theta1)),
+        y: y10 + (length * Math.cos(theta1)),
+        mass: 2
+    };
+    var circle2 = {
+        x: x10 + length * Math.sin(theta1) + length * Math.sin(theta2),
+        y: y10 + length * Math.cos(theta1) + length * Math.cos(theta2),
+        mass: 2
+    };
+    
+    circle1.x = x10 + length * Math.sin(theta1);
+    circle1.y = y10 + length * Math.cos(theta1);
+    circle2.x = x10 + length * Math.sin(theta1) + length * Math.sin(theta2);
+    circle2.y = y10 + length * Math.cos(theta1) + length * Math.cos(theta2);
+    
+    line1.x  = circle1.x;
+    line1.y  = circle1.y;
+    line2.x0 = circle1.x;
+    line2.y0 = circle1.y;
+    line2.x  = circle2.x;
+    line2.y  = circle2.y;
+    
+    cxt.clearRect(0, 0, canv.width, canv.height);
+
+    drawLine(line1, cxt);
+    drawLine(line2, cxt);
+    drawCircle(circle1, cxt);
+    drawCircle(circle2, cxt);
+}
+
+function beginSim(canv, cxt) {
     var line1 = {
         name: "1",
         x0: x10,
@@ -111,43 +166,71 @@ function beginSim(canv, cxt, init) {
     }, 10);
 }
 
-function resizeCanvas() {
-    canvs.width = window.innerWidth * 0.75;
-    canvs.height = window.innerHeight * 0.80;
-    x10 = (canvs.width * 0.5) - 8;
-    y10 = (canvs.height * 0.33) - 8;
+function resizeCanvas(canvs, context) {
+    var canWidth = window.innerWidth * 0.86;
+    var canHeight = window.innerHeight * 0.76;
+    
+    canvs.width = canWidth;
+    canvs.height = canHeight;
+    
+    x10 = parseInt(((canvs.width * 0.5) - 6));
+    y10 = parseInt(((canvs.height * 0.33) - 6));
+    
+    return ((Math.min(canWidth, canHeight) / 4) - 6);
 }
 
-function getSliderVals() {
-    poten1 = $('#pot1').val();
-    poten2 = $('#pot2').val();
-    theta1 = $('#phet1').val();
-    theta2 = $('#phet2').val();
-    mass = $('#mass1').val();
-    time = $('#tSteps').val();
+function getSliderVals(ln) {
+    g = 9.81;
+    length = ln;
+    gOvL = g / length;
+    mass = 10;
+    mass = parseInt($('#mass1').val());
+    mpht = 6 / (mass * Math.pow(length, 2));
+    mpot = -0.5 * (mass * Math.pow(length, 2));
+    
+    time = parseFloat($('#tSteps').val());
+    
+    poten1 = parseInt($('#pot1').val());
+    poten2 = parseInt($('#pot2').val());
+    theta1 = parseInt($('#phet1').val()) / 180 * Math.PI;
+    theta2 = parseInt($('#phet2').val()) / 180 * Math.PI;
+    
+    fOrdPoten1 = 0;
+    fOrdPoten2 = 0;
+    fOrdTheta1 = 0;
+    fOrdTheta2 = 0;
+}
+
+function submitHandler(can, cxt, l) {
+    $('#myPanel').panel('close');
+    getSliderVals(l);
+    can  = document.getElementById('pendCanvas');
+    cxt = can.getContext('2d');
+    beginSim(can, cxt);
 }
 
 $(document).ready(function (){
-    var init = {};    
-    canvs = document.getElementById('pendCanvas');
-    contxt = canvs.getContext('2d');
+    var canvs  = document.getElementById('pendCanvas');
+    var contxt = canvs.getContext('2d');
+    var lngth = 0;
     
-    resizeCanvas();
+    $('#myPanel').on('panelbeforeopen', function(event, ui) {
+        drawInitialBars(canvs, contxt);
+    });
     
-    window.addEventListener('resize', resizeCanvas, false);
-    
-    $('.sliders').change(function() {
-        var selOfText = "#" + $(this).attr('id') + "V";
-        $(selOfText).val($(this).val());
+    $( ".sliders" ).on('slidestop', function(event) {
+        drawInitialBars(canvs, contxt);
     });
     
     $('#btnSub').click(function() {
-        init = {};
-        getSliderVals();
-        canvs  = document.getElementById('pendCanvas');
-        contxt = canvs.getContext('2d');
-        beginSim(canvs, contxt, init);
+        submitHandler(canvs, contxt, lngth);
     });
     
+    $('#btnSub').bind('tap', function() {
+        submitHandler(canvs, contxt, lngth);
+    });
+    
+    lngth = resizeCanvas(canvs, contxt);
+    getSliderVals(lngth);    
     beginSim(canvs, contxt, init);
 });

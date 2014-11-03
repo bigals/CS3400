c7661d55fa1a7022f86db782cc4a42bfea270adc
bigals <bigalpromos@gmail.com>
2014-11-03 15:28:26 -0500

    Working on linking, and passing credentials between pages, need to look at local storage.

--- FinalProject/Scripts/script.js
+++ FinalProject/Scripts/script.js
@@ -1,32 +1,50 @@
-'use strict';
-//wrap all javascript logic in an anonymous function
-(function() {
-    //Global Login Credentials Object
-    var loginCred = {
-        loginUname: " ",
-        loginPass: " "
-    };
 
-    function onLoginSubmit() {
-        var emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-        
-        loginCred.loginUname = $("input[name='loginEmail']").val();
-        loginCred.loginPass = $("input[name='loginPass']").val();
-        
-        //Check that email has a valid input.... TODO: Need to add check for password here
-        if(emailRegEx.test(loginCred.loginEmail))
-        {
-            location.href = "./home.html";
-        }
-        else
-        {
-            //need error handleing for invalid username
-        }
+
+function onLoginSubmit() {
+    var url = $.url(document.location);
+
+    loginCred.loginUname = $("#loginUName").val();
+    loginCred.loginPass = $("#loginPass").val();
+
+    //Check that email has a valid input.... TODO: Need to add check for password here
+    $.mobile.navigate("./home.html", {data:{uName:loginCred.loginUname, uPass:loginCred.loginPass}});
+    //location.href = "./home.html?uName=" + loginCred.loginUname + "&uPass=" + loginCred.loginPass;
+}
+
+function parseParamsFromQueryString() {
+    var url = $.url(document.location);
+
+    var uNm = url.param("uName");
+    if (uNm != undefined) {
+        loginCred.loginUname = uNm;
     }
+    var uPass = url.param("uPass");
+    if (uPass != undefined) {
+        loginCred.loginPass = uPass;
+    }
+    return url;
+}
 
-    $(document).ready(function () {
-        $('#loginSubmit').click(function() {
-            onLoginSubmit();
-        });
+//JQuery Mobile Equivilent of $(document).ready()
+$(document).on('pageinit', function() {
+    parseParamsFromQueryString();
+    
+    $('#loginSubmit').click(function() {
+        onLoginSubmit();
     });
-})();
+    
+    $('a.navBLinks').on('click', function() {
+        parseParamsFromQueryString();
+        switch($(this).text())
+        {
+                case 'Repos':                
+                    $.mobile.navigate("./repos.html", {data:{uName:loginCred.loginUname, uPass:loginCred.loginPass}});
+                    break;
+        }
+    });
+    
+    $('a.repoDetailLink').on('click', function() {
+        var index = $(this).attr('id').substring(5);
+        $.mobile.navigate("./detailedRepos.html", {data:{uName:loginCred.loginUname, uPass:loginCred.loginPass}});        
+    });
+});
